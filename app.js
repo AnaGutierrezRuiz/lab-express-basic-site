64618f06a1ae97a211d6c5e8888d686f46d1e0dc
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@Anas-MacBook-Air.local>
2023-01-17 18:14:47 +0100

    added some html content to the views files.

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ app.set('views', `${__dirname}/views`);
 
 app.get('/', (req, res, next) => {
   console.log('this is home');
-  res.render('home');
+  res.render('home', {
+    name: name
+  });
 })
 
 app.get('/about', (res, req, next) => {
   console.log('this is about');
-  res.render('about');
+  res.render('about', {
+    name: name
+  });
 })
 
 app.get('/works', (res, req, next) => {
   console.log('this is works');
-  res.render('works');
+  res.render('works', {
+    name: name
+  });
 })
 
 const port = 3000;
