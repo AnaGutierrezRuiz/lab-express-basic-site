61c59518b0eaeb860ee6524c1b843f2f1068bbed
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@Anas-iMac.local>
2023-01-18 12:17:29 +0100

    started implementing styles, added gallery.hbs

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', `${__dirname}/views`);
 
+app.use(express.static('public'));
+
 app.get('/', (req, res, next) => {
   console.log('this is home');
+  const name = 'Canela';
   res.render('home', {
     name: name
   });
@@ -13,6 +16,7 @@ app.get('/', (req, res, next) => {
 
 app.get('/about', (res, req, next) => {
   console.log('this is about');
+  const name = 'Canela';
   res.render('about', {
     name: name
   });
@@ -20,9 +24,18 @@ app.get('/about', (res, req, next) => {
 
 app.get('/works', (res, req, next) => {
   console.log('this is works');
+  const name = 'Canela';
   res.render('works', {
     name: name
   });
+})
+
+app.get('/gallery', (req, res, next) => {
+  console.log('this is gallery');
+  const name = 'Canela';
+  res.render('works', {
+    name: name
+  })
 })
 
 const port = 3000;
